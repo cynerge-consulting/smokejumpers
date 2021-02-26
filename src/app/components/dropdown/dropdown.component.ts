03f37f80b5427fae75d048f0e3d80d0b66e9e43c
Alex Petroski <alexpetroski@gmail.com>
2021-02-26 09:04:14 -0500

    Dropdown somewhat working, not fully completed

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent implements OnInit {
+  isOpen: boolean = false;
+  isSelected: boolean = false;
+  @Input() placeholder: string;
+  @Input() required: boolean;
+  @Input() selectedValue: any;
+  @Input() dropdownOptions = [
+    {
+      name: 'Angular',
+      value: 'angular'
+    },
+    {
+      name: 'Vue',
+      value: 'vue'
+    },
+    {
+      name: 'React',
+      value: 'react'
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('document:click', ['$event.target']) public onClick(target) {
+    const clickedInside = this.elementRef.nativeElement.contains(target);
+    if (!clickedInside) {
+      this.isOpen = false;
+    }
+    console.log(this.isOpen);
   }
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+    console.log(this.isOpen);
+  }
+
+  changeOption(event: any): void {
+    console.log(event);
+    this.selectedValue = event.target.value;
+    this.placeholder = event.target.innerText;
+    this.isOpen = false;
+  }
 }
