2bd0d404fa1eb3bbccc165e5c1c005f501bc5b12
brian davidson <brian.davidson.1981@gmail.com>
2021-02-26 12:49:21 -0500

    2 - added a check to see if the path exists

--- env.js
+++ env.js
@@ -5,15 +5,24 @@ let production = false;
 let API_URL = 'http://localhost:6977';
 // don't touch anything below here
 
-let path = process.cwd()
-path += production ? '/src/environments/environment.prod.ts' : '/src/environments/environment.ts';
+let pwd = process.cwd();
+let path = pwd;
+path += production
+  ? '/src/environments/environment.prod.ts'
+  : '/src/environments/environment.ts';
 
 let template = `export const environment = {
   production: ${production},
   API_URL: '${API_URL}'
-};`
+};`;
 
-fs.writeFile(path, template, function (err) {
-  if (err) return console.log(err);
+if (!fs.existsSync(pwd + '/src/environments')){
+    fs.mkdirSync(pwd + '/src/environments');
+}
+
+fs.writeFile(path, template, {flag: 'w+'}, function (err) {
+  if (err) {
+    return console.log(err);
+  }
   console.log('Environment Variables were generated successfully!');
 });
