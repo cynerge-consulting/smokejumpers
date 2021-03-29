fe4ff09eaac3f103733b50ce54860c5d280262c2
Alex Petroski <alexpetroski@gmail.com>
2021-03-29 19:43:25 -0400

    Removed testing code

--- src/app/routes/incidents/incidents.component.ts
+++ src/app/routes/incidents/incidents.component.ts
@@ -80,7 +80,6 @@ export class IncidentsComponent implements OnInit {
         console.dir(error);
         this.toast.show('Unable to retreive incidents.', 'error');
       });
-    this.toast.show('Example toast', 'warning');
   }
 
   confirmDeleteIncident = (incident) => {
