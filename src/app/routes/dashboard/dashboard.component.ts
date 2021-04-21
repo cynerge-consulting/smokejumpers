fd0687c4e7187cde9203d9720594f98a2be03966
brian davidson <brian.davidson.1981@gmail.com>
2021-04-21 02:31:22 -0400

    dashboard page

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -7,7 +7,48 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  cards = [
+    {
+      title: 'Booster in Brief',
+      description: 'Information about Boosters',
+      route: 'booster'
+    },
+    {
+      title: 'Incidents',
+      description: 'View Incidents across bases',
+      route: 'incidents'
+    },
+    {
+      title: 'Parachutes',
+      description: 'Information about Parachutes',
+      route: 'chutes'
+    },
+    {
+      title: 'Jumpers',
+      description: 'View and Manage Jumpers',
+      route: 'jumpers'
+    },
+    {
+      title: 'Aircraft',
+      description: 'Information about Aircraft',
+      route: 'aircraft'
+    },
+    {
+      title: 'Pilots',
+      description: 'View Pilot Information',
+      route: 'pilots'
+    },
+    {
+      description: 'Review Daily and Annual Reports',
+      title: 'Reports',
+      route: 'reports'
+    }
+  ];
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  go = (route) => {
+    this.router.navigate([route]);
+  };
 }
